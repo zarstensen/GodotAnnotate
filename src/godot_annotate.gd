f7fa1e0eb0baf1ad84659196b11f27bd937ae048
karstensensensen <simonblsoerensen@gmail.com>
2024-02-19 21:08:27 +0100

    Replaced constants with variables which are loaded and unloaded on plugin enable and disable.

--- src/godot_annotate.gd
+++ src/godot_annotate.gd
@@ -9,43 +9,64 @@ static var selected_canvas: AnnotateCanvas
 
 static var poly_in_progress := false
 
-const CANVAS_IMAGE_DIALOG_SCENE := preload("res://addons/GodotAnnotate/res/CanvasImageDialog.tscn")
-const UPSCALE_FACTOR_DIALOG_SCENE := preload("res://addons/GodotAnnotate/res/UpscaleFactorDialog.tscn")
-const CANVAS_TOOLBAR_SCENE := preload("res://addons/GodotAnnotate/res/annotate_2d_toolbar_control.tscn")
-
-static var canvas_menu_control: Control
+static var canvas_image_dialog: FileDialog
+static var upscale_factor_dialog: ConfirmationDialog
+static var canvas_toolbar: Control
 
 static var editor_interface: EditorInterface
 
 func _enter_tree():
 	
+	# initialize variables
+	
+	canvas_image_dialog = preload("res://addons/GodotAnnotate/res/CanvasImageDialog.tscn").instantiate()
+	upscale_factor_dialog = preload("res://addons/GodotAnnotate/res/UpscaleFactorDialog.tscn").instantiate()
+	canvas_toolbar = preload("res://addons/GodotAnnotate/res/annotate_2d_toolbar_control.tscn").instantiate()
+	canvas_toolbar.visible = false
+	
+	poly_in_progress = false
+	
+	# setup signals
+	
 	EditorInterface.get_selection().selection_changed.connect(_on_selection_changed)
+	
+	upscale_factor_dialog.confirmed.connect(func():
+		EditorInterface.popup_dialog_centered(canvas_image_dialog)
+	)
+	
+	canvas_image_dialog.file_selected.connect(func(file):
+		selected_canvas._on_capture_canvas(file,
+				upscale_factor_dialog.get_node("UpscaleFactorInput").value
+				)
+	)
 	
 	add_custom_type("AnnotateCanvas",
 			"Node2D",
 			preload("res://addons/GodotAnnotate/src/annotate_canvas.gd"),
 			preload("res://addons/GodotAnnotate/annotate_layer_icon.svg"))
 	
-	# setup toolbar
-	
-	canvas_menu_control = CANVAS_TOOLBAR_SCENE.instantiate()
-	canvas_menu_control.visible = false
-	
 	# connect button signals
 	
-	var toggle_annotate: Button = canvas_menu_control.get_node("ToggleAnnotateButton")
+	var toggle_annotate: Button = canvas_toolbar.get_node("ToggleAnnotateButton")
 	
-	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, canvas_menu_control)
+	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, canvas_toolbar)
 	
 	editor_interface = get_editor_interface()
 
 func _exit_tree():
-	remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, canvas_menu_control)
-	canvas_menu_control.queue_free()
+	remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, canvas_toolbar)
+	canvas_toolbar.queue_free()
 	
 	remove_custom_type("AnnotateCanvas")
 	
 	EditorInterface.get_selection().selection_changed.disconnect(_on_selection_changed)
+	
+	# free variables
+	
+	canvas_image_dialog.queue_free()
+	upscale_factor_dialog.queue_free()
+	canvas_toolbar.queue_free()
+	
 
 ## Forwards relevant 2d editor user inputs to an [AnnotateCanvas] node.
 ## TODO: clean this up a bit.
@@ -57,21 +78,7 @@ func _forward_canvas_gui_input(event):
 		
 		# canvas capture (shortcut: shift + alt + s)
 		if event.keycode == KEY_S && event.pressed && event.alt_pressed && event.shift_pressed:
-			var upscale_factor_dialog: ConfirmationDialog = UPSCALE_FACTOR_DIALOG_SCENE.instantiate()
-			upscale_factor_dialog.confirmed.connect(func():
-				
-				var canvas_image_dialog: FileDialog = UPSCALE_FACTOR_DIALOG_SCENE.instantiate()
-				canvas_image_dialog.file_selected.connect(func(file):
-					
-					# upscale factor is present in the spinbox child of the upscale factor dialog.
-					selected_canvas._on_capture_canvas(file, upscale_factor_dialog.get_child(0).value)
-				
-				)
-				
-				get_editor_interface().popup_dialog_centered(canvas_image_dialog)
-			
-			)
-			
+			upscale_factor_dialog.get_node("UpscaleFactorInput").value = 1
 			get_editor_interface().popup_dialog_centered(upscale_factor_dialog)
 		
 		# polygon drawing
@@ -126,11 +133,11 @@ func _handles(object):
 	return object is AnnotateCanvas
 
 func _on_selection_changed():
-	canvas_menu_control.visible = false
+	canvas_toolbar.visible = false
 	selected_canvas = null
 	
 	var nodes := EditorInterface.get_selection().get_selected_nodes()
 	
 	if len(nodes) == 1 and nodes[0] is AnnotateCanvas:
-		canvas_menu_control.visible = true
+		canvas_toolbar.visible = true
 		selected_canvas = nodes[0] as AnnotateCanvas
