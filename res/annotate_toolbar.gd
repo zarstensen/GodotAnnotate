f5f3b3767b7d3c2ead84b75b8a8be0e399a9167d
Zarstensen <61250140+zarstensen@users.noreply.github.com>
2024-03-01 23:20:01 +0100

    Implemented freehand stroke and annotate mode. (#31)

--- res/annotate_toolbar.gd
+++ res/annotate_toolbar.gd
@@ -3,12 +3,10 @@ extends HBoxContainer
 
 var canvas: AnnotateCanvas
 
-var canvas_image_dialog: FileDialog = preload("res://addons/GodotAnnotate/res/CanvasImageDialog.tscn").instantiate()
-var upscale_factor_dialog: ConfirmationDialog = preload("res://addons/GodotAnnotate/res/UpscaleFactorDialog.tscn").instantiate()
+var canvas_image_dialog: FileDialog = preload("res://addons/GodotAnnotate/res/canvas_image_dialog.tscn").instantiate()
+var upscale_factor_dialog: ConfirmationDialog = preload("res://addons/GodotAnnotate/res/upscale_factor_dialog.tscn").instantiate()
 
 func _ready() -> void:
-	$AnnotateMode.text = ""
-	
 	upscale_factor_dialog.confirmed.connect(func() -> void:
 			EditorInterface.popup_dialog_centered(canvas_image_dialog)
 			)
@@ -16,6 +14,13 @@ func _ready() -> void:
 	canvas_image_dialog.file_selected.connect(func(path: String) -> void:
 			canvas._on_capture_canvas(path, upscale_factor_dialog.get_node("UpscaleFactorInput").value)
 			)
+			
+	# setup annotate mode dropdown
+	
+	for annotate_mode in GodotAnnotate.annotate_modes:
+		$AnnotateMode.add_icon_item(load(annotate_mode.get_icon_path()), annotate_mode.get_mode_name())
+	
+	$AnnotateMode.text = ""
 
 # Called when a new annotate canvas node is selected.
 func _on_new_canvas(new_canvas: AnnotateCanvas) -> void:
@@ -36,3 +41,4 @@ func _on_toggle_annotate_button_toggled(toggled_on: bool) -> void:
 
 func _on_annotate_mode_item_selected(index: int) -> void:
 	$AnnotateMode.text = ""
+	canvas._annotate_mode = GodotAnnotate.annotate_modes[index]
--- src/annotate_canvas.gd
+++ src/annotate_canvas.gd
@@ -1,9 +1,14 @@
 @tool
+@icon("res://addons/GodotAnnotate/annotate_layer_icon.svg")
 class_name AnnotateCanvas
 extends Node2D
 ##
 ## Node allowing user to paint and view [AnnotateStroke]s on a [AnnotateLayer] in the 2D editor.
 ##
+
+## Imports
+const AnnotateMode := preload("res://addons/GodotAnnotate/src/annotate_mode.gd")
+const Stroke := preload("res://addons/GodotAnnotate/src/stroke.gd")
 
 ## Percentage size increase to stroke size caused by shift + scroll.
 const SIZE_SCROLL_PERC: float = 0.1
@@ -46,26 +51,33 @@ var min_point_distance = 0.25
 @export
 var layer_resource: AnnotateLayer = AnnotateLayer.new()
 
+## Annotate mode currently being used on the canvas.
+var _annotate_mode: AnnotateMode = GodotAnnotate.annotate_modes[0]
 ## Stroke currently being painted by the user.
-var _active_stroke: AnnotateStrokeLine
+var _active_stroke: Node2D
 ## [code] true [/code] if user is currently trying to erase strokes.
 var _erasing := false
 
-## Array of [AnnotateStrokeLine]s, which all visually represents all of the [AnnotateStroke] resources
-## in the layer_resource array.
-var _stroke_lines: Array[AnnotateStrokeLine] = [ ]
+@export
+## Array of Strokes currently stored in the canvas.
+var _strokes: Array[PackedScene] = [ ]
 
-func get_canvas_area() -> Rect2:
+## Return the smallest possible Rect2 Which contains all the passed Rect2's.
+func _merge_rects(rects: Array[Rect2]) -> Rect2:
 	
-	if layer_resource.strokes.size() <= 0:
+	if len(rects) <= 0:
 		return Rect2()
 	
-	var canvas_area := layer_resource.strokes[0].boundary
-	
-	for stroke in layer_resource.strokes.slice(1):
-		canvas_area = canvas_area.merge(stroke.boundary)
+	var final_rect = rects[0]
+
+	for rect in rects.slice(1):
+		final_rect = final_rect.merge(rect)
 		
-	return canvas_area
+	return final_rect
+
+## Return the smallest Rect2, which contains all the strokes currently stored in the canvas
+func get_canvas_area() -> Rect2:
+	return _merge_rects(_strokes.map(func(s): s.boundary))
 
 func _ready():
 	if not Engine.is_editor_hint() and not show_when_running:
@@ -73,38 +85,10 @@ func _ready():
 	
 	# restore lines from previously saved state.
 	
-	for stroke in layer_resource.strokes:
-		var line := AnnotateStrokeLine.from_stroke(stroke)
-		add_child(line)
-		_stroke_lines.append(line)
+	for stroke in _strokes:
+		add_child(stroke.instantiate())
 
-func _on_begin_stroke():
-	_active_stroke = AnnotateStrokeLine.new(brush_size / 100 * max_brush_size, brush_color)
-	add_child(_active_stroke)
-	_stroke_lines.append(_active_stroke)
-	# instantly insert a point, to avoid the user having to drag the cursor,
-	# in order to insert a point.
-	_active_stroke.try_annotate_point(get_local_mouse_position(), min_point_distance, true)
-
-func _on_end_stroke():
-	if !GodotAnnotate.poly_in_progress:
-		# force insert final point, as the stroke should end where the user stopped the stroke,
-		# even if the final point is within AnnotateStroke.MIN_POINT_DISTANCE.
-		_active_stroke.try_annotate_point(get_local_mouse_position(), min_point_distance, true)
-		
-	layer_resource.strokes.append(_active_stroke.to_stroke())
-	_active_stroke = null
-	
-func _on_begin_erase():
-	_erasing = true
-
-func _on_end_erase():
-	_erasing = false
-
-func _on_draw_poly_stroke():
-	_active_stroke.try_annotate_point(get_local_mouse_position(), min_point_distance, false)
-
-func _on_stroke_resize(direction: float):
+func resize_stroke(direction: float):
 	brush_size *= 1 + direction * SIZE_SCROLL_PERC
 	brush_size = min(100, max(brush_size, 1))
 
@@ -112,14 +96,14 @@ func _on_capture_canvas(file: String, scale: float):
 	add_child(AnnotateCanvasCaptureViewport.new(self, file, scale))
 
 func _process(delta):
-	if _active_stroke && !GodotAnnotate.poly_in_progress:
-		_active_stroke.try_annotate_point(get_local_mouse_position(), min_point_distance, false)
+	if _active_stroke:
+		_annotate_mode.on_annotate_process(delta, _active_stroke, self)
 		
 	if _erasing:
 		var erase_stroke_indexes: Array[int] = []
 		
-		for i in range(_stroke_lines.size()):
-			if _stroke_lines[i].collides_with(get_local_mouse_position(), brush_size / 100 * max_brush_size):
+		for i in range(get_child_count()):
+			if get_child(i).collides_with_circle(get_local_mouse_position(), brush_size / 100 * max_brush_size):
 				erase_stroke_indexes.append(i)
 		
 		for erase_count in range(erase_stroke_indexes.size()):
@@ -128,20 +112,76 @@ func _process(delta):
 			
 			var remove_index := erase_stroke_indexes[erase_count] - erase_count
 			
-			layer_resource.strokes.remove_at(remove_index)
-			_stroke_lines[remove_index].queue_free()
-			_stroke_lines.remove_at(remove_index)
+			get_child(remove_index).queue_free()
+			_strokes.remove_at(remove_index)
 		
 	queue_redraw()
 
 func _draw():
 	if lock_canvas:
 		return
-	
-	if GodotAnnotate.poly_in_progress:
-		draw_dashed_line(_active_stroke.points[-1], get_local_mouse_position(), brush_color, brush_size * 0.125, brush_size * 0.25)
-	
+
 	if _erasing:
-		draw_arc(get_local_mouse_position(), brush_size / 100 * max_brush_size / 2, 0, TAU, 32, Color.INDIAN_RED, 3, true)
+		draw_arc(get_local_mouse_position(),
+				brush_size / 100 * max_brush_size,
+				0, TAU, 32, Color.INDIAN_RED, 3, true)
+	
 	elif GodotAnnotate.selected_canvas == self:
-		draw_circle(get_local_mouse_position(), brush_size / 100 * max_brush_size / 2, brush_color)
+		_annotate_mode.draw_cursor(self,
+				get_local_mouse_position(),
+				brush_size / 100 * max_brush_size,
+				brush_color)
+	
+	#if GodotAnnotate.poly_in_progress:
+		#draw_dashed_line(_active_stroke.points[-1], get_local_mouse_position(), brush_color, brush_size * 0.125, brush_size * 0.25)
+	#elif GodotAnnotate.selected_canvas == self:
+		#draw_circle(get_local_mouse_position(), brush_size / 100 * max_brush_size / 2, brush_color)
+
+func on_editor_input(event: InputEvent) -> bool:
+
+	if _active_stroke:
+		
+		if _annotate_mode.should_end_stroke(event):
+			
+			_annotate_mode.on_end_stroke(get_local_mouse_position(), _active_stroke, self)
+			
+			var scene = PackedScene.new()
+			scene.pack(_active_stroke)
+			
+			_strokes.append(scene)
+			
+			_active_stroke = null
+			
+			return true
+			
+		return _annotate_mode.on_annotate_input(event)
+	
+	elif event is InputEventMouseButton:
+		# erasing
+		if event.button_index == MOUSE_BUTTON_RIGHT && event.pressed:
+			_erasing = true
+			return true
+		elif event.button_index == MOUSE_BUTTON_RIGHT && not event.pressed:
+			_erasing = false
+			return true
+		
+		# stroke size (shift + scroll)
+		# cannot use ctrl or alt, since they control view position and zoom,
+		# and cannot be prevented from being forwarded by returning true.
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN && Input.is_key_pressed(KEY_SHIFT):
+			if event.pressed:
+				resize_stroke(-1)
+			
+			return true
+		elif event.button_index == MOUSE_BUTTON_WHEEL_UP && Input.is_key_pressed(KEY_SHIFT):
+			if event.pressed:
+				resize_stroke(1)
+			
+			return true
+	
+	if _annotate_mode.should_begin_stroke(event):
+		_active_stroke = _annotate_mode.on_begin_stroke(get_local_mouse_position(), brush_size, brush_color, self)
+		add_child(_active_stroke)
+		return true
+	
+	return false
